0d725ae342ccc5de2ec5d9162ebc1cd6a6b131ab
Alireza Alidousti <alireza.eva.u23@gmail.com>
2017-10-15 18:00:27 -0700

    formats comments and removes unused func

--- index.js
+++ index.js
@@ -1,90 +1,57 @@
- // Import the following modules and service key
-var admin = require('firebase-admin');
-var gameEngine = require('./engine');
-// TODO: you need to create a firebase project and download the service key into a folder in the root and name it cred 
-var serviceAccount = require('./cred/serviceAccountKey.json');
+// Import the following modules and service key
+const admin = require('firebase-admin');
+const gameEngine = require('./engine');
 
-// Init the firebase app using following credentails
-const fireDatabaseUrl = "https://production-artris.firebaseio.com/" //TODO: insert your firebase database url here
+// TODO: you need to create a firebase project and
+//       download the service key into a folder in the root and name it cred
+const serviceAccount = require('./cred/serviceAccountKey.json');
+// TODO: insert your firebase database url here
+const fireDatabaseUrl = 'https://production-artris.firebaseio.com/';
+
+// Init the firebase app
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: fireDatabaseUrl //
+  databaseURL: fireDatabaseUrl
 });
+
 // Get firebase database refrence
-var rootDbRef = admin.database();
+const rootDbRef = admin.database();
 // Global que that manages the state
-var queue = {};
-// Lock bit to know when not to read data from the queue
-var lock = 0;
+let queue = {};
+// Lock bit to know when to read data from the queue
+let lock = 0;
 // Keep track of dirty values
-var dirtyValues = {};
-// Look up table for the game sessions 
-var gamesSessions = {};
+let dirtyValues = {};
+// Look up table for the game sessions
+let gamesSessions = {};
 
-
-//Listen for all changes to get when the gesture changes
-rootDbRef.ref('game-session/').on('child_changed', snapshot => {
-  extractDataFromObject(snapshot.val(), (playerId, playerAction) => {
-      // console.log(playerId);
-      // console.log(playerAction);
-    });
-  },
+// Listen for all changes to get when the gesture changes
+rootDbRef.ref('game-session/').on(
+  'child_changed',
+  snapshot => {},
   error => {
     console.error(`Some error has occured :${error}`);
   }
 );
 
-//Listen for game start when a node is added to the game-session/
-rootDbRef.ref('game-session/').once('child_added',snapshot => {
-  // init the game and add it to a look up table and run it
+// Listen for game start when a node is added to the game-session/
+rootDbRef.ref('game-session/').once('child_added', snapshot => {
+  // Init the game and add it to a look up table and run it
   gamesSessions[snapshot.key] = new gameEngine.init();
-  // console.log(gamesSessions[snapshot.key]);
   gamesSessions[snapshot.key].run();
-  // console.log( gamesSessions[snapshot.key].util);
-  gamesSessions[snapshot.key].util.on("newEngineState",(newGameState)=>{
-    jsonPreprocessor(newGameState, (cells) =>{
-      updateTetrisGrid(snapshot.key,Object.keys(newGameState)[0], cells);
+  gamesSessions[snapshot.key].util.on('newEngineState', newGameState => {
+    jsonPreprocessor(newGameState, cells => {
+      updateTetrisGrid(snapshot.key, Object.keys(newGameState)[0], cells);
     });
-  })
-}) 
-
-
-
-/**
- * Copied form hack for handeling gestures
- * @param {Post process the data recived from firebase} data 
- * @param {Callback with the players id and the action they have made} callback 
- */
-function extractDataFromObject(data, callback) {
-  // process all the players in the game
-  // Object.keys(data).forEach(key => {
-  //   // Get the id of the last item that was added
-  //   var latestDetailId = Object.keys(data[key]).pop();
-  //   var tempObj = {};
-  //   // if the key is not dirty
-  //   if (!(latestDetailId in dirtyValues)) {
-  //     // Get the core detils from the added object
-  //     var detail = data[key][latestDetailId];
-  //     tempObj[key] = detail;
-  //   }
-  //   //add the key to a dictionary of dirty values
-  //   dirtyValues[latestDetailId] = true;
-  //   // return the object to the master queue if it's not empty
-  //   if (Object.keys(tempObj).length > 0) {
-  //     // send the player and the details of the move back
-  //     callback(key, tempObj[key]);
-  //   }
-  //   // return tempObj;
-  // });
-}
-
+  });
+});
 
 let jsonPreprocessor = function(data, callback) {
-  cells = [];
+  let cells = [];
 
-  Object.values(data).map(block => {
+  Object.values(data).forEach(block => {
     pos = block.position;
-    block.shape.map(cell => {
+    block.shape.forEach(cell => {
       cells.push({
         x: pos.x + cell.x,
         y: pos.y + cell.y,
@@ -93,11 +60,10 @@ let jsonPreprocessor = function(data, callback) {
       });
     });
   });
-
   callback(cells);
-}
+};
 
-let updateTetrisGrid = function (gameId, blockId, data) {
-  // add passed in block ot the render node
-  rootDbRef.ref(`game-session/${gameId}/grid-render/${blockId}`).set(data)
-}
+let updateTetrisGrid = function(gameId, blockId, data) {
+  // Add passed in block ot the render node
+  rootDbRef.ref(`game-session/${gameId}/grid-render/${blockId}`).set(data);
+};
